06d0ea02b227a97f0f384ecb9708635be52a99c0
Wellington Moreno <wellington@censio.love>
2019-02-21 12:16:19 -0800

    Refines copyright code

--- src/main/java/com/cloudkitchens/Shelf.kt
+++ src/main/java/com/cloudkitchens/Shelf.kt
@@ -14,21 +14,8 @@
  * limitations under the License.
  */
 
-package com.cloudkitchens/*
- * Copyright 2019 SirWellington
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+package com.cloudkitchens
+
 
 import com.cloudkitchens.ShelfType.OVERFLOW
 import com.cloudkitchens.Temperature.COLD
@@ -41,22 +28,6 @@ import tech.sirwellington.alchemy.annotations.designs.patterns.StrategyPattern
 import tech.sirwellington.alchemy.annotations.designs.patterns.StrategyPattern.Role.CONCRETE_BEHAVIOR
 import tech.sirwellington.alchemy.annotations.designs.patterns.StrategyPattern.Role.INTERFACE
 import java.util.concurrent.LinkedBlockingDeque
-
-/*
- * Copyright 2019 SirWellington
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 /**
  * Where orders are placed once they are ready for delivery.
