83f5932cae1b4606353762f56fe90c4f3748589f
Wellington Moreno <wellington@censio.love>
2019-02-21 12:16:29 -0800

    Creates a scheduler for handling kitchen events

---
+++ src/main/java/com/cloudkitchens/Scheduler.kt
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2019 SirWellington
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.cloudkitchens
+
+import tech.sirwellington.alchemy.kotlin.extensions.createListOf
+import java.util.concurrent.Executors
+import java.util.concurrent.ScheduledExecutorService
+import java.util.concurrent.TimeUnit
+
+
+/**
+ *
+ * @author SirWellington
+ */
+class Scheduler(private val orderGenerator: () -> OrderRequest,
+                private val poissonGenerator: PoissonGenerator,
+                private val λ: Double = 3.25,
+                private val scheduler: ScheduledExecutorService = Executors.newScheduledThreadPool(2),
+                private val kitchen: Kitchen)
+{
+    private val LOG = getLogger()
+
+    fun begin()
+    {
+        val command = Runnable { run() }
+        scheduler.scheduleAtFixedRate(command, 0, 1, TimeUnit.SECONDS)
+    }
+
+    fun stop()
+    {
+        scheduler.shutdownNow()
+    }
+
+    fun run()
+    {
+        val newOrderCount = poissonGenerator.getPoisson(λ)
+        val newOrders = createListOf(newOrderCount, orderGenerator)
+
+        val orders = newOrders.forEach { kitchen.receiveOrder(it) }
+
+        LOG.info("Added [$newOrderCount] new orders to the kitchen")
+    }
+}
