b32afab49bcefbbcb73d9982d51043fe29d77bbb
Wellington Moreno <wellington@censio.love>
2019-02-22 21:45:22 -0800

    Adds test code

--- src/test/java/com/cloudkitchens/KitchenSimulationTest.kt
+++ src/test/java/com/cloudkitchens/KitchenSimulationTest.kt
@@ -16,17 +16,30 @@
 
 package com.cloudkitchens
 
+import com.natpryce.hamkrest.and
+import com.natpryce.hamkrest.assertion.assertThat
+import com.natpryce.hamkrest.equalTo
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.Mock
+import tech.sirwellington.alchemy.kotlin.extensions.random
+import tech.sirwellington.alchemy.test.hamcrest.notNull
 import tech.sirwellington.alchemy.test.junit.runners.AlchemyTestRunner
+import java.io.File
 
 @RunWith(AlchemyTestRunner::class)
 class KitchenSimulationTest
 {
 
     private lateinit var instance: KitchenSimulation
+
+    @Mock
+    private lateinit var display: Display
+
+    @Mock
+    private lateinit var poissonGenerator: PoissonGenerator
 
     @Before
     fun setup()
@@ -43,36 +56,56 @@ class KitchenSimulationTest
     @Test
     fun testBegin()
     {
+        instance.begin()
     }
 
     @Test
     fun testStop()
     {
+        instance.begin()
+
+        Thread.sleep(100)
+
+        instance.stop()
     }
 
     @Test
     fun testWithDisplay()
     {
+        val result = instance.withDisplay(display)
+        assertThat(result, notNull and equalTo(instance))
     }
 
     @Test
     fun testWithDisplayToFile()
     {
+        val file = File.createTempFile("something", ".json")
+        val result = instance.withDisplayToFile(file)
+        assertThat(result, notNull and equalTo(instance))
     }
 
     @Test
     fun testWithPoissonGenerator()
     {
+        val result = instance.withPoissonGenerator(poissonGenerator)
+        assertThat(result, notNull and equalTo(instance))
     }
 
     @Test
     fun testWithPoissonLambda()
     {
+        val λ = Double.random(1.0, 10.0)
+        val result = instance.withPoissonLambda(λ)
+        assertThat(result, notNull and equalTo(instance))
     }
 
     @Test
     fun testWithTrafficDelayRange()
     {
+        val min = Int.random(1, 10)
+        val max = Int.random(min + 1, 200)
+        val result = instance.withTrafficDelayRange(min..max)
+        assertThat(result, notNull and equalTo(instance))
     }
 
 }
